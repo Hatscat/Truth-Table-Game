19c7521bd74e052e26e3050f3761db045d0ca953
hatscat <luboudy@gmail.com>
2017-12-09 15:36:13 +0100

    add game level singleton

---
+++ src/main/scala/com/ttg/game/singleton/GameLevel.scala
@@ -0,0 +1,30 @@
+package com.ttg.game.singleton
+
+object GameLevel {
+
+  lvl = 0
+
+  private var _lvl: Int = 0
+  private var _variablesQuantity: Int = 0
+  private var _rowsQuantity: Int = 0
+  private var _expectedResult: Int = 0
+
+  def variablesQuantity: Int = _variablesQuantity
+
+  def rowsQuantity: Int = _rowsQuantity
+
+  def expectedResult: Int = _expectedResult
+
+  def lvl: Int = _lvl
+
+  def lvl_=(n: Int): Unit = {
+    _lvl = n
+    _variablesQuantity = 1 + log2(log2(_lvl - oneRotOneRotSum(log2(log2(_lvl)))))
+    _rowsQuantity = 1 << _variablesQuantity
+    _expectedResult = _lvl - oneRotOneRotSum(_variablesQuantity)
+  }
+
+  private def log2(n: Int): Int = n.toBinaryString.length - 1
+
+  private def oneRotOneRotSum(i: Int): Int = ((i - 1) to 1 by -1).fold(0) { (a, b) => a + (1 << (1 << b)) }
+}
